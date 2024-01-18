64e59290952858333435e25be3b13d9d8691381a
Mustafa2 <Mustafauyar91@gmail.com>
2024-01-18 23:53:44 +0300

    recyclerView resim ekledim

--- app/src/main/java/com/mustafauyar/superkahramankitabi/RecyclerAdapter.kt
+++ app/src/main/java/com/mustafauyar/superkahramankitabi/RecyclerAdapter.kt
@@ -2,9 +2,11 @@ package com.mustafauyar.superkahramankitabi
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import org.w3c.dom.Text
@@ -27,8 +29,17 @@ class RecyclerAdapter(
     }
 
     override fun onBindViewHolder(holder: SuperKahramanVH, position: Int) {
+        /*
+        todo :
+            1- resimleri ikon haline getir
+            2- açılan yeni activity içine de recyclerView ekle bu sefer de bu kahramanın benzer resimlerini göstersin
+            3 - birde birtane döngü yaz daha önceki derslerde ki gibi
+
+         */
         val kahramanListesi = kahramanHashMap.keys.toList()
+        val cacheImage =BitmapFactory.decodeResource(holder.itemView.context.resources,kahramanHashMap[kahramanListesi[position]] ?:R.drawable.joker)
         holder.itemView.findViewById<TextView>(R.id.recyclerViewTextView).text = kahramanListesi[position]
+        holder.itemView.findViewById<ImageView>(R.id.kahramanImage).setImageBitmap(cacheImage)
 
         holder.itemView.setOnClickListener {
             val intent = Intent(holder.itemView.context,TanitimActivity::class.java)
