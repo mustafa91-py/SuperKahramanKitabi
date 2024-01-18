7c3379f1ed1f7ab98c26a1c266cdcc55ae9112cb
Mustafa2 <Mustafauyar91@gmail.com>
2024-01-18 13:48:52 +0300

    ders6_3 verileri hazirlamak

--- app/src/main/java/com/mustafauyar/superkahramankitabi/MainActivity.kt
+++ app/src/main/java/com/mustafauyar/superkahramankitabi/MainActivity.kt
@@ -1,4 +1,6 @@
 package com.mustafauyar.superkahramankitabi
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import com.mustafauyar.superkahramankitabi.databinding.ActivityMainBinding
 
 import androidx.appcompat.app.AppCompatActivity
@@ -11,5 +13,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        var superKahramanListesi = ArrayList<String>()
+        superKahramanListesi.add("ironman")
+        superKahramanListesi.add("batman")
+        superKahramanListesi.add("joker")
+        superKahramanListesi.add("samurai jack")
+        superKahramanListesi.add("dört kol")
+        val ironmanBitmap = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.ironman)
+        val batmanBitmap = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.batman)
+        val jokerBitmap = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.joker)
+        val samuraiJackBitmap = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.samurai_jack)
+        val fourArmBitmap = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.four_arm)
+        val superKahramanBitmaps = ArrayList<Bitmap>()
+        superKahramanBitmaps.add(ironmanBitmap)
+        superKahramanBitmaps.add(batmanBitmap)
+        superKahramanBitmaps.add(jokerBitmap)
+        superKahramanBitmaps.add(samuraiJackBitmap)
+        superKahramanBitmaps.add(fourArmBitmap)
     }
 }
